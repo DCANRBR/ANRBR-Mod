8ea07c1215cf3108598e9b763c983997174c99c9
DCANRBR <anrbr@danishcrownc.com>
2023-02-08 12:50:34 +0100

    Added Copy-ModuleToUserMods function and module manifest file

--- testscript.ps1
+++ testscript.ps1
@@ -1,5 +1,14 @@
-Import-Module ".\ANRBR-Mod.PSM1" -Force
+Import-Module ".\ANRBR-Mod.PSD1" -Force
 
+
+Copy-ModuleToUserMods -ModulePSMPath '.\ANRBR-Mod.PSM1'
+<#
 Measure-Command {
 Get-Process | Export-ExcelANRBR -Path '.\testsheet.xlsx' -TimeStamp
 }
+
+
+$myPath = "C:\Users\anrbr\Downloads"
+
+$items = Get-ChildItem -Path $myPath -Recurse -Force | Measure-Object -Property
+#>
